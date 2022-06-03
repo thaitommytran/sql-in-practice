36c1a2d1e36da46ee75ec209ee563a0b17b9c6f0
Thai Tran <thai.tommy.tran@gmail.com>
2022-06-03 14:25:49 -0500

    initial commit

---
+++ server/controller.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const { CONNECTION_STRING } = process.env;
+
+let nextEmp = 5;
+
+module.exports = {
+  getUpcomingAppointments: (req, res) => {
+    sequelize
+      .query(
+        `select a.appt_id, a.date, a.service_type, a.approved, a.completed, u.first_name, u.last_name 
+        from cc_appointments a
+        join cc_emp_appts ea on a.appt_id = ea.appt_id
+        join cc_employees e on e.emp_id = ea.emp_id
+        join cc_users u on e.user_id = u.user_id
+        where a.approved = true and a.completed = false
+        order by a.date desc;`
+      )
+      .then((dbRes) => res.status(200).send(dbRes[0]))
+      .catch((err) => console.log(err));
+  },
+
+  approveAppointment: (req, res) => {
+    let { apptId } = req.body;
+
+    sequelize
+      .query(
+        `*****YOUR CODE HERE*****
+        
+        insert into cc_emp_appts (emp_id, appt_id)
+        values (${nextEmp}, ${apptId}),
+        (${nextEmp + 1}, ${apptId});
+        `
+      )
+      .then((dbRes) => {
+        res.status(200).send(dbRes[0]);
+        nextEmp += 2;
+      })
+      .catch((err) => console.log(err));
+  }
+};
